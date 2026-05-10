44423beb04bc583e2cfadabc952631a431480bc8
Akachukwu Aneke <akalodave@gmail.com>
2026-05-10 16:28:25 +0100

    fix

--- app/Http/Resources/ClientBondRequestResource.php
+++ app/Http/Resources/ClientBondRequestResource.php
@@ -5,7 +5,7 @@ namespace app\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-use App\Http\Resources\ClientBondResource;
+use app\Http\Resources\ClientBondResource;
 
 class ClientBondRequestResource extends JsonResource
 {
