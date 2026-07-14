f2d3347e35ef51be008f939b77e1427cacef6550
Akachukwu Aneke <akalodave@gmail.com>
2026-07-14 09:40:53 +0100

    feat: add SALES role to Roles enum and database seeder

--- app/Enums/Roles.php
+++ app/Enums/Roles.php
@@ -3,12 +3,13 @@
 namespace app\Enums;
 
 enum Roles: string
-    {
-        case SUPER_ADMIN = "super admin";
-        case ADMIN = "admin";
-        case HUMAN_RESOURCE = "human resource";
-        case LOGISTICS = "logistics";
-        case CUSTOMER_RELATION = "customer relation";
-        case OPERATION_ACCOUNTING = "operation and accounting";
-        case CONTENT_MANAGEMENT = "content management";
-    }
+{
+    case SUPER_ADMIN = "super admin";
+    case ADMIN = "admin";
+    case HUMAN_RESOURCE = "human resource";
+    case LOGISTICS = "logistics";
+    case CUSTOMER_RELATION = "customer relation";
+    case OPERATION_ACCOUNTING = "operation and accounting";
+    case CONTENT_MANAGEMENT = "content management";
+    case SALES = "sales";
+}
